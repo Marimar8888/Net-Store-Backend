ee86eda9f0afcced19aac4a73184f871f65b0599
Marimar <malonso@quadram.mobi>
2023-05-23 13:20:15 +0200

    02.Bakend - Configuración base de datos en memoria

---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace net_store_backend.Domain.Entities
+
+{
+    [Table("categories")]
+    public class Category
+    {
+        public long Id { get; set; }
+        [Column(TypeName = "vachar(100)")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+        [Column(TypeName = "varchar(2000)")]
+        public string? Description { get; set; }
+        [Required]
+        public byte[] Image { get; set; }
+    }
+}
---
+++ Domain/Entities/Item.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace net_store_backend.Domain.Entities
+{
+    public class Item
+    {
+        public long Id { get; set; }
+        [Column(TypeName = "vachar(100)")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+        [Column(TypeName = "varchar(2000)")]
+        public string? Description { get; set; }
+        [Range(0, double.MaxValue)]
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public byte[] Image { get; set; }
+        [Required]
+        public long CategoryId { get; set; }
+        [Required]
+        public Category Category { get; set; }
+    }
+}
+
